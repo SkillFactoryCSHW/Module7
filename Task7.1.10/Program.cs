eb79bb1cd467e14f1125dfccfaa560d7043fe889
Bochanikita <bochanikita@yandex.ru>
2022-09-26 15:34:54 +0300

    Add task 7.1.10

---
+++ Task7.1.10/Program.cs
@@ -0,0 +1,25 @@
+﻿class BaseClass
+{
+    protected string Name;
+
+    public BaseClass(string name)
+    {
+        Name = name;
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public string Description;
+    public int Counter;
+
+    public DerivedClass(string name, string description) : base(name)
+    {
+        Description = description;
+    }
+
+    public DerivedClass(string name, string description, int counter) : this(name, description)
+    {
+        Description = description;
+    }
+}
