a4c28433e3976759b35e0564823d242f986feae4
Bochanikita <bochanikita@yandex.ru>
2022-09-26 15:12:22 +0300

    Add Task 7.1.5

---
+++ Task7.1.5/Program.cs
@@ -0,0 +1,38 @@
+﻿class Food
+{
+
+}
+class Vegetables : Food
+{
+
+}
+
+class Fruit : Food
+{
+
+}
+
+class Apple : Fruit
+{
+
+}
+
+class Banana : Fruit
+{
+
+}
+
+class Pear : Fruit
+{
+
+}
+
+class Potato : Vegetables
+{
+
+}
+
+class Carrot : Vegetables
+{
+
+}
