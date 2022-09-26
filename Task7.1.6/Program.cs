497aa7667fe5f1a97738806b31fd64fe1d918127
Bochanikita <bochanikita@yandex.ru>
2022-09-26 15:21:57 +0300

    Add task 7.1.6

---
+++ Task7.1.6/Program.cs
@@ -0,0 +1,15 @@
+﻿class Obj
+{
+	private string name;
+	private string owner;
+	private int length;
+	private int count;
+
+	public Obj(string name, string ownerName, int objLength, int count)
+	{
+		this.name = name;
+		this.owner = ownerName;
+		this.length = objLength;
+		this.count = count;
+	}
+}
