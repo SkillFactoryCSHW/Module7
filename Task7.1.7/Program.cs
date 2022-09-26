f70bf5ab99fdd00d1dbad194a86d56656e51c56c
Bochanikita <bochanikita@yandex.ru>
2022-09-26 15:24:46 +0300

    Add task 7.1.7

---
+++ Task7.1.7/Program.cs
@@ -0,0 +1,25 @@
+﻿class SmartHelper
+{
+    private string name;
+
+    public SmartHelper(string name)
+    {
+        this.name = name;
+    }
+
+    public void Greetings(string name)
+    {
+        Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name );
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SmartHelper helper = new SmartHelper("Олег");
+        helper.Greetings("Грег");
+
+        Console.ReadKey();
+    }
+
+}
