b3f335428318b1f9fe73e151c03fc9809f38c8b2
Bochanikita <bochanikita@yandex.ru>
2022-09-27 22:37:35 +0300

    Add task 7.2.5

---
+++ Task7.2.5/Program.cs
@@ -0,0 +1,16 @@
+﻿class BaseClass
+{
+    public virtual void Display()
+    {
+        Console.WriteLine("BaseClass method");
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine("DerivedClass method"); 
+    }
+}
