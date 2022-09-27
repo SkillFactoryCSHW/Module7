dc09c1a1f421402c0d1aaa47c838355068e9abea
Bochanikita <bochanikita@yandex.ru>
2022-09-27 23:01:23 +0300

    Add task 7.2.7

---
+++ Task 7.2.7/Program.cs
@@ -0,0 +1,39 @@
+﻿class A
+{
+    public virtual void Display()
+    {
+        Console.WriteLine("A");
+    }
+}
+
+class B : A
+{
+    public new void Display()
+    {
+        Console.WriteLine("B");
+    }
+}
+
+class C : A
+{
+    public override void Display()
+    {
+        Console.WriteLine("C");
+    }
+}
+
+class D : B
+{
+    public void Display()
+    {
+        Console.WriteLine("D");
+    }
+}
+
+class E : C
+{
+    public void Display()
+    {
+        Console.WriteLine("E");
+    }
+}
