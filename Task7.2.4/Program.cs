22d60fff4e8394615921f8eab2ae4af38e960cba
Bochanikita <bochanikita@yandex.ru>
2022-09-27 19:58:48 +0300

    Add task 7.2.4

---
+++ Task7.2.4/Program.cs
@@ -0,0 +1,32 @@
+﻿class BaseClass
+{
+    public virtual int Counter
+    {
+        get;
+        set;
+    }
+}
+
+class DerivedClass: BaseClass
+{
+    public int num;
+
+    public override int Counter
+    {
+        get
+        {
+            return num;
+        }
+        set
+        {
+            if(num < 0)
+            {
+                Console.WriteLine("Num can be more then 0 only");
+            }
+            else
+            {
+                num = value;
+            }
+        }
+    }
+}
