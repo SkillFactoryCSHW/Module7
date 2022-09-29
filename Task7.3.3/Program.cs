afe09b2bbc71ba5d47ade2f7664e9bd077ed25f7
Bochanikita <bochanikita@yandex.ru>
2022-09-29 23:11:06 +0300

    Add task 7.3.3

---
+++ Task7.3.3/Program.cs
@@ -0,0 +1,18 @@
+﻿abstract class ComputerPart
+{
+    public abstract void Work();
+}
+class Processor : ComputerPart
+{
+    public override void Work() { }
+}
+
+class MotherBoard : ComputerPart
+{
+    public override void Work() { }
+}
+
+class GraphicCard : ComputerPart
+{
+    public override void Work() { }
+}
