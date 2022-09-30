e3d47a9a2344b0e9938169f21ff61206ae9b2091
Bochanikita <bochanikita@yandex.ru>
2022-09-30 15:05:43 +0300

    Add task 7.5.5

---
+++ Task 7.5.5/Program.cs
@@ -0,0 +1,16 @@
+﻿class Obj
+{
+    public string Name;
+    public string Description;
+
+    public static string Parent;
+    public static int DaysInWeek;
+    public static int MaxValue;
+
+    static Obj()
+    {
+        Parent = "System.Object";
+        DaysInWeek = 7;
+        MaxValue = 2000;
+    }
+}
